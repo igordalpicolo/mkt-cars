a9e0e351381757bf01027da3618fb441bc899be4
Igor Dal Picolo <igordalpicolo11@hotmail.com>
2025-02-10 21:20:55 -0300

    Create project

---
+++ src/main/java/com/igordalpicolo/mkt_cars/MktCarsApplication.java
@@ -0,0 +1,13 @@
+package com.igordalpicolo.mkt_cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MktCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MktCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igordalpicolo/mkt_cars/MktCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igordalpicolo.mkt_cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MktCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
